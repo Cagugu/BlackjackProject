06a58a19733f445e514b8cd713344cf5d608a5d8
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-03-11 16:27:29 -0600

    added classes from labs

---
+++ src/com/skilldistillery/cards/common/Suit.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.cards.common;
+
+public enum Suit {
+	HEARTS("Hearts"), SPADES("Spades"), CLUBS("Clubs"), DIAMONDS("Diamonds");
+	
+	private String name;
+
+	private Suit(String name) {
+		this.name = name;
+	}
+	
+	public String toString() {
+		return name;
+		
+	}
+}
